ac042289d60914217f623231904ff7f4d0c9a6eb
patsicko <patsicko@g>
2022-06-27 04:47:00 +0200

    Feat:Toggle

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 
 
 let link=document.getElementsByTagName("p");
+console.log(link);
 let lang=Array.from(link);
 console.log(lang);
 for(let i=0, c=lang.length; i<c;i++){
   lang[i].addEventListener("click",function(e){
   console.log(e);
-    e.target.nextElementSibling.style.visibility="visible";
-    
-  })
-}
-for(let i=0, c=lang.length; i<c;i++){
-  lang[i].addEventListener("dblclick",function(e){
-  console.log(e);
-    e.target.nextElementSibling.style.visibility="hidden";
+    e.target.nextElementSibling.classList.toggle("show");
     
   })
 }
 
 
+// for(let i=0, c=lang.length; i<c;i++){
+//   lang[i].addEventListener("dblclick",function(e){
+//   console.log(e);
+//     e.target.nextElementSibling.style.visibility="hidden";
+    
+//   })
+// }
+
+let btn=document.getElementById("btn");
+let aside=document.getElementById("aside");
+let shadow=document.getElementById("shadow");
+let navigations=document.getElementById("navigations");
+
+btn.addEventListener("click",function(e){
+  aside.classList.toggle("hide");
+  shadow.classList.toggle("cover");
+  navigations.classList.toggle("expand");
+})
